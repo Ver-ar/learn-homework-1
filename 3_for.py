b699816ba7d221944bdb91a884ce3f9f7b5b9702
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2021-11-26 18:13:18 +0300

    Sales_mistakes-1

--- 3_for.py
+++ 3_for.py
@@ -4,7 +4,7 @@
 
 Цикл for: Продажи товаров
 
-* Дан список словарей с данными по колличеству проданных телефонов
+* Дан список словарей с данными по количеству проданных телефонов
   [
     {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
@@ -17,11 +17,44 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    sales = [
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 
+    480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 
+    211, 354, 276, 526, 141, 453, 510, 316]},
+    {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 
+    437, 214, 494, 441, 518, 212, 288, 272, 247]},
+  ]
+  
+    def count_phone_sold(phone_items_sold):
+        items_sold_sum = 0
+        for item_sold in phone_items_sold:
+            items_sold_sum += item_sold
+        return count_phone_sold
+        
+    def count_phone_sold_avg(phone_items_sold):
+        items_sold_sum = 0
+        for item_sold in phone_items_sold:
+            items_sold_sum += item_sold
+            count_phone_sold_avg = round(items_sold_sum / len(phone_items_sold),2)
+        return count_phone_sold_avg
+        
+        
+    for phone in sales:
+        sales_sum=0
+        count_phone_sold = count_phone_sold(phone['items_sold'])
+        count_phone_sold_avg = count_phone_sold_avg(phone['items_sold'])
+        
+        print(f'Среднее количество продаж {phone["product"]}: {count_phone_sold_avg}')
+        print(f'Суммарное количество продаж {phone["product"]}: {count_phone_sold}')
+        
+        sales_sum += count_phone_sold(phone['items_sold'])
+        sales_sum_avg = round(sales_sum / len(sales[phone]), 2)
+        
+    
+    print(f'Среднее количество всех продаж: {sales_sum}')
+    print(f'Суммарное количество всех продаж: {sales_sum_avg}')
+    
     
 if __name__ == "__main__":
     main()
