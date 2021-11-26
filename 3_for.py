3f582cc3af9e853766982b73bfff057697f40c5a
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2021-11-26 19:12:59 +0300

    Change_2

--- 3_for.py
+++ 3_for.py
@@ -18,32 +18,29 @@
 
 def main():
   sales = [
-  {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 
-  480, 476, 470, 216, 270, 388, 312, 186]}, 
-  {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 
-  211, 354, 276, 526, 141, 453, 510, 316]},
-  {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 
-  437, 214, 494, 441, 518, 212, 288, 272, 247]},
-]
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+    {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+  ]
   
   def count_phone_sold(phone_items_sold):
     items_sold_sum = 0
     for item_sold in phone_items_sold:
       items_sold_sum += item_sold
     return count_phone_sold
+  
         
   def count_phone_sold_avg(phone_items_sold):
     items_sold_sum = 0
     for item_sold in phone_items_sold:
       items_sold_sum += item_sold
-      count_phone_sold_avg = round(items_sold_sum / len(phone_items_sold),2)
-    return count_phone_sold_avg
+    return round(items_sold_sum / len(phone_items_sold), 2)
         
   sales_sum = 0
   for phone in sales:
         
-    count_phone_sold = count_phone_sold(phone['items_sold'])
-    count_phone_sold_avg = count_phone_sold_avg(phone['items_sold'])
+    count_phone_sold = count_phone_sold (phone['items_sold'])
+    count_phone_sold_avg = count_phone_sold_avg (phone['items_sold'])
         
     print(f'Среднее количество продаж {phone["product"]}: {count_phone_sold_avg}')
     print(f'Суммарное количество продаж {phone["product"]}: {count_phone_sold}')
