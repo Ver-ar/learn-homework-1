553bf369e2aeb70d21002565b992cd5606102090
Ver-ar <94222049+Ver-ar@users.noreply.github.com>
2021-11-27 00:40:27 +0300

    Kak_dela_poka_done

--- 6_exception1.py
+++ 6_exception1.py
@@ -10,11 +10,26 @@
     
 """
 
+from typing import MutableMapping
+
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+
+  try:
+
+    while True:
+
+      question = str(input('Как дела?\n'))
+
+      if question == "Хорошо":
+        break
+
+      else:      
+        question = str(input('Как дела?\n'))
+        
+  except KeyboardInterrupt:
+    print('Пока!')
+  
+        
+        
 if __name__ == "__main__":
-    hello_user()
+  hello_user()
